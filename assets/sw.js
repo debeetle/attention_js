53481d857e531ab5a8c7afba02a90c1fbbc48087
chaos <chaos@learning>
2026-06-06 17:06:33 +0800

    feat: upgrade UI transitions with spring easing and implement service worker cache versioning

--- assets/sw.js
+++ assets/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attention';
+const CACHE_NAME = 'attention-v1';
 const ASSETS = [
     '/',
     '/index.html',
@@ -15,6 +15,20 @@ const ASSETS = [
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
